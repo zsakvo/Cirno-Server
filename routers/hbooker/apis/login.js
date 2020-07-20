d4430a8479e4e60adc2dc4f94424dfd545965565
zsakvo <gdwqsp@gmail.com>
2020-07-20 17:08:47 +0800

    clean code

--- routers/hbooker/apis/login.js
+++ routers/hbooker/apis/login.js
@@ -1,6 +1,6 @@
-const parse = require("./mixin/parse-res");
-const get = require("../../libs/http").get;
-const fix = require("./mixin/fix-param");
+const parse = require("../mixin/parse-res");
+const get = require("../../../libs/http").get;
+const fix = require("../mixin/fix-param");
 
 let login = async function (name, passwd) {
   let res = await get({
