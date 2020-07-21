d8bed843fb10e6b859bf60d1460f00197fe73dc7
zsakvo <gdwqsp@gmail.com>
2020-07-21 10:36:13 +0800

    allow cors

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const Koa = require("koa");
+const cors = require("koa2-cors");
 const app = new Koa();
 const Router = require("koa-router");
 const bodyParser = require("koa-bodyparser");
 const fs = require("fs");
+
+//允许跨域
+app.use(cors());
 
 //读取 tokens
 allTokens = {};
